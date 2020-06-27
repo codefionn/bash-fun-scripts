f5a48a8ca1a5c803d389b2234613a8b84cca36c2
Fionn Langhans <fionn.langhans@gmail.com>
2020-06-27 16:08:10 +0000

    Update sqlbackup.sh

--- sqlbackup.sh
+++ sqlbackup.sh
@@ -4,6 +4,10 @@ if [ -z "$1" ] ; then
 	exit 1;
 fi
 
+# Backups mysql/mariadb $1 to MEGA
+# A SQL file is created in /tmp and then compressed
+# Afterwards the file is uploaded and then both files are being removed
+
 OUT_FILE_PLAIN=/tmp/sqldump-$1_`date +"%Y%m%d-%H%M%S"`.sql
 OUT_FILE="${OUT_FILE_PLAIN}.tar.xz"
 UPLOAD_DIR=/Root/sqldump/
